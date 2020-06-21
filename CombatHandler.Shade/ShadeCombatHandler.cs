9957294265ac85e4011ac90c115883f457fddbcb
Phaz <phaz@phaz-online.com>
2020-06-21 12:23:02 -0500

    Adjusted perk logic so chains are prioritized over queuing max number of perks

--- CombatHandler.Shade/ShadeCombatHandler.cs
+++ CombatHandler.Shade/ShadeCombatHandler.cs
@@ -14,6 +14,39 @@ namespace Desu
         private const int DOF_BUFF = 210159;
         private const int LIMBER_BUFF = 210158;
 
+        private List<PerkHash> TotemicRites = new List<PerkHash>
+        {
+            PerkHash.RitualOfDevotion,
+            PerkHash.DevourVigor,
+            PerkHash.RitualOfZeal,
+            PerkHash.DevourEssence,
+            PerkHash.RitualOfSpirit,
+            PerkHash.DevourVitality,
+            PerkHash.RitualOfBlood
+        };  
+
+        private List<PerkHash> PiercingMastery = new List<PerkHash>
+        {
+            PerkHash.Stab,
+            PerkHash.DoubleStab,
+            PerkHash.Perforate,
+            PerkHash.Lacerate,
+            PerkHash.Impale,
+            PerkHash.Gore,
+            PerkHash.Hecatomb
+        };
+
+        private List<PerkHash> SpiritPhylactery = new List<PerkHash>
+        {
+            PerkHash.CaptureVigor,
+            PerkHash.UnsealedBlight,
+            PerkHash.CaptureEssence,
+            PerkHash.UnsealedPestilence,
+            PerkHash.CaptureSpirit,
+            PerkHash.UnsealedContagion,
+            PerkHash.CaptureVitality
+        };
+
         public ShadeCombatHandler() : base()
         {
             //Perks
@@ -22,27 +55,9 @@ namespace Desu
 
             RegisterPerkProcessor(PerkHash.Blur, GenericDamagePerk);
 
-            RegisterPerkProcessor(PerkHash.CaptureVigor, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.UnsealedBlight, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.CaptureEssence, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.UnsealedPestilence, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.CaptureSpirit, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.UnsealedContagion, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.CaptureVitality, GenericDamagePerk);
-
-            RegisterPerkProcessor(PerkHash.Perforate, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.Lacerate, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.Impale, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.Gore, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.Hecatomb, GenericDamagePerk);
-
-            RegisterPerkProcessor(PerkHash.RitualOfDevotion, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.DevourVigor, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.RitualOfZeal, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.DevourEssence, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.RitualOfSpirit, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.DevourVitality, GenericDamagePerk);
-            RegisterPerkProcessor(PerkHash.RitualOfBlood, GenericDamagePerk);
+            SpiritPhylactery.ForEach(p => RegisterPerkProcessor(p, SpiritPhylacteryPerk));
+            TotemicRites.ForEach(p => RegisterPerkProcessor(p, TotemicRitesPerk));
+            PiercingMastery.ForEach(p => RegisterPerkProcessor(p, PiercingMasteryPerk));
 
             RegisterPerkProcessor(PerkHash.Bore, GenericDamagePerk);
             RegisterPerkProcessor(PerkHash.Crave, GenericDamagePerk);
@@ -57,7 +72,7 @@ namespace Desu
 
         private bool Limber(Perk perk, SimpleChar fightingTarget, out SimpleChar target)
         {
-            target = fightingTarget;
+            target = null;
 
             Buff dof;
             if (DynelManager.LocalPlayer.Buffs.Find(DOF_BUFF, out dof) && dof.RemainingTime > 12.5f)
@@ -68,7 +83,7 @@ namespace Desu
 
         private bool DanceOfFools(Perk perk, SimpleChar fightingTarget, out SimpleChar target)
         {
-            target = fightingTarget;
+            target = null;
 
             Buff limber;
             if (!DynelManager.LocalPlayer.Buffs.Find(LIMBER_BUFF, out limber) || limber.RemainingTime > 12.5f)
@@ -86,5 +101,82 @@ namespace Desu
 
             return true;
         }
+
+        private bool PiercingMasteryPerk(Perk perk, SimpleChar fightingTarget, out SimpleChar target)
+        {
+            target = fightingTarget;
+
+            if (fightingTarget == null)
+                return false;
+
+            //Don't PM if there are TR/SP chains in progress
+            if (_actionQueue.Any(x => x.CombatAction is Perk action && (TotemicRites.Contains(action.Hash) || SpiritPhylactery.Contains(action.Hash))))
+                return false;
+
+            if (!(Perk.Find(PerkHash.Stab, out Perk stab) && Perk.Find(PerkHash.DoubleStab, out Perk doubleStab)))
+                return true;
+
+            if (perk.Hash == PerkHash.Perforate)
+            {
+                if (_actionQueue.Any(x => x.CombatAction is Perk action && (action == stab || action == doubleStab)))
+                    return false;
+            }
+
+            if (!(Perk.Find(PerkHash.Stab, out Perk perforate) && Perk.Find(PerkHash.DoubleStab, out Perk lacerate)))
+                return true;
+
+            if (perk.Hash == PerkHash.Impale)
+            {
+                if (_actionQueue.Any(x => x.CombatAction is Perk action && (action == stab || action == doubleStab || action == perforate || action == lacerate)))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool SpiritPhylacteryPerk(Perk perk, SimpleChar fightingTarget, out SimpleChar target)
+        {
+            target = fightingTarget;
+
+            if (fightingTarget == null)
+                return false;
+
+            //Don't SP if there are TR/PM chains in progress
+            if (_actionQueue.Any(x => x.CombatAction is Perk action && (TotemicRites.Contains(action.Hash) || PiercingMastery.Contains(action.Hash))))
+                return false;
+
+            return true;
+        }
+
+        private bool TotemicRitesPerk(Perk perk, SimpleChar fightingTarget, out SimpleChar target)
+        {
+            target = fightingTarget;
+
+            if (fightingTarget == null)
+                return false;
+
+            //Don't TR if there are SP/PM chains in progress
+            if (_actionQueue.Any(x => x.CombatAction is Perk action && (SpiritPhylactery.Contains(action.Hash) || PiercingMastery.Contains(action.Hash))))
+                return false;
+
+            return true;
+        }
+
+        private IEnumerable<PerkHash> GetTotemicRitesPerks()
+        {
+            yield return PerkHash.RitualOfDevotion;
+            yield return PerkHash.DevourVigor;
+
+
+            RegisterPerkProcessor(PerkHash.RitualOfDevotion, GenericDamagePerk);
+            RegisterPerkProcessor(PerkHash.DevourVigor, GenericDamagePerk);
+            RegisterPerkProcessor(PerkHash.RitualOfZeal, GenericDamagePerk);
+            RegisterPerkProcessor(PerkHash.DevourEssence, GenericDamagePerk);
+            RegisterPerkProcessor(PerkHash.RitualOfSpirit, GenericDamagePerk);
+            RegisterPerkProcessor(PerkHash.DevourVitality, GenericDamagePerk);
+            RegisterPerkProcessor(PerkHash.RitualOfBlood, GenericDamagePerk);
+        }
+
+
     }
 }

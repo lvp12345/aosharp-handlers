0d66d68ce9a774665f23a290d416dcc43a4b87de
Phaz <phaz@phaz-online.com>
2020-06-28 13:01:29 -0500

    Added radar project

---
+++ Radar/Radar.cs
@@ -0,0 +1,44 @@
+﻿using AOSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing;
+using System.Linq;
+using AOSharp.Common.GameData;
+using AOSharp.Core.Inventory;
+using AOSharp.Core.Movement;
+
+namespace Radar
+{
+    public class Main : IAOPluginEntry
+    {
+        public void Run(string pluginDir)
+        {
+            try
+            {
+                Chat.WriteLine("Radar loaded!");
+                Game.OnUpdate += OnUpdate;
+            }
+            catch (Exception e)
+            {
+                Chat.WriteLine(e.Message);
+            }
+        }
+
+        private void OnUpdate(object sender, float e)
+        {
+            DrawPlayers();
+        }
+
+        private void DrawPlayers()
+        {
+            foreach (SimpleChar player in DynelManager.Players)
+            {
+                if (player.Identity == DynelManager.LocalPlayer.Identity)
+                    continue;
+
+                Debug.DrawSphere(player.Position, 1, DebuggingColor.Red);
+                Debug.DrawLine(DynelManager.LocalPlayer.Position, player.Position, DebuggingColor.Red);
+            }
+        }
+    }
+}

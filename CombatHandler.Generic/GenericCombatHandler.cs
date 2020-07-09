f83ffd7be62d0ad4952947c181bb58fb1df3e78c
Phaz <phaz@phaz-online.com>
2020-07-08 23:15:20 -0500

    Added a few items to combat handler

--- CombatHandler.Generic/GenericCombatHandler.cs
+++ CombatHandler.Generic/GenericCombatHandler.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using AOSharp.Common.GameData;
 using AOSharp.Core;
 using AOSharp.Core.Combat;
 using AOSharp.Core.Inventory;
+using AOSharp.Core.UI;
 
 namespace CombatHandler.Generic
 {
@@ -36,21 +38,22 @@ namespace CombatHandler.Generic
 
             // Opportunity knocks
 
-
-            //RegisterItemProcessor(245990, 245990, DamageItem);
+            RegisterItemProcessor(RelevantItems.FlurryOfBlowsLow, RelevantItems.FlurryOfBlowsLow, DamageItem);
+            RegisterItemProcessor(RelevantItems.FlurryOfBlowsHigh, RelevantItems.FlurryOfBlowsHigh, DamageItem);
+            RegisterItemProcessor(RelevantItems.StrengthOfTheImmortal, RelevantItems.StrengthOfTheImmortal, DamageItem);
+            RegisterItemProcessor(RelevantItems.MightOfTheRevenant, RelevantItems.MightOfTheRevenant, DamageItem);
+            RegisterItemProcessor(RelevantItems.BarrowStrength, RelevantItems.BarrowStrength, DamageItem);
+            RegisterItemProcessor(RelevantItems.LavaCapsule, RelevantItems.LavaCapsule, DamageItem);
+            RegisterItemProcessor(RelevantItems.KizzermoleGumboil, RelevantItems.KizzermoleGumboil, DamageItem);
+            RegisterItemProcessor(RelevantItems.SteamingHotCupOfEnhancedCoffee, RelevantItems.SteamingHotCupOfEnhancedCoffee, Coffee);
 
             // xp stim?
             // health and nano recharger
             // first aid stim
             // free movement
-            // flurry
-            // barrow strength/motr (include 220 totw one)
             // absorb shoulders (include 220 totw one)
             // endurance booster
-            // kizzer
-            // lava cap
             // totw rings?
-            // coffee
             // reani cloaks
             // deflection shield
             // SoM stims
@@ -113,7 +116,55 @@ namespace CombatHandler.Generic
             if (fightingTarget == null)
                 return false;
 
+            if (DynelManager.LocalPlayer.Cooldowns.ContainsKey(GetSkillLockStat(item)))
+                return false;
+
             return true;
+        }
+
+        protected virtual bool Coffee(Item item, SimpleChar fightingTarget, out SimpleChar target)
+        {
+            target = null;
+
+            if (!DynelManager.LocalPlayer.Buffs.Contains(Nanoline.FoodandDrinkBuffs))
+                return DamageItem(item, fightingTarget, out _);
+
+            return false;
+        }
+
+        // This will eventually be done dynamically but for now I will implement
+        // it statically so we can have it functional
+        private Stat GetSkillLockStat(Item item)
+        {
+            switch (item.HighId)
+            {
+                case RelevantItems.FlurryOfBlowsLow:
+                case RelevantItems.FlurryOfBlowsHigh:
+                    return Stat.AggDef;
+                case RelevantItems.StrengthOfTheImmortal:
+                case RelevantItems.MightOfTheRevenant:
+                case RelevantItems.BarrowStrength:
+                    return Stat.Strength;
+                case RelevantItems.LavaCapsule:
+                case RelevantItems.KizzermoleGumboil:
+                    return Stat.SharpObject;
+                case RelevantItems.SteamingHotCupOfEnhancedCoffee:
+                    return Stat.RunSpeed;
+                default:
+                    throw new Exception($"No skill lock stat defined for item id {item.HighId}");
+            }
+        }
+
+        private static class RelevantItems
+        {
+            public const int FlurryOfBlowsLow = 85907;
+            public const int FlurryOfBlowsHigh = 85908;
+            public const int StrengthOfTheImmortal = -1;
+            public const int MightOfTheRevenant = 206013;
+            public const int BarrowStrength = 204653;
+            public const int LavaCapsule = 245990;
+            public const int KizzermoleGumboil = 245323;
+            public const int SteamingHotCupOfEnhancedCoffee = 157296;
         }
     }
 }

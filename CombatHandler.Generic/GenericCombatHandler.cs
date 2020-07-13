4cdbeacf378b8ed4536603b87c4688070d871f78
Phaz <phaz@phaz-online.com>
2020-07-12 22:07:40 -0500

    Added meteorite spikes as an alternative to kizzer/lava capsule

--- CombatHandler.Generic/GenericCombatHandler.cs
+++ CombatHandler.Generic/GenericCombatHandler.cs
@@ -43,10 +43,13 @@ namespace CombatHandler.Generic
             RegisterItemProcessor(RelevantItems.StrengthOfTheImmortal, RelevantItems.StrengthOfTheImmortal, DamageItem);
             RegisterItemProcessor(RelevantItems.MightOfTheRevenant, RelevantItems.MightOfTheRevenant, DamageItem);
             RegisterItemProcessor(RelevantItems.BarrowStrength, RelevantItems.BarrowStrength, DamageItem);
-            RegisterItemProcessor(RelevantItems.LavaCapsule, RelevantItems.LavaCapsule, DamageItem);
-            RegisterItemProcessor(RelevantItems.KizzermoleGumboil, RelevantItems.KizzermoleGumboil, DamageItem);
+            RegisterItemProcessor(RelevantItems.MeteoriteSpikes, RelevantItems.MeteoriteSpikes, TargetedDamageItem);
+            RegisterItemProcessor(RelevantItems.LavaCapsule, RelevantItems.LavaCapsule, TargetedDamageItem);
+            RegisterItemProcessor(RelevantItems.KizzermoleGumboil, RelevantItems.KizzermoleGumboil, TargetedDamageItem);
             RegisterItemProcessor(RelevantItems.SteamingHotCupOfEnhancedCoffee, RelevantItems.SteamingHotCupOfEnhancedCoffee, Coffee);
 
+            RegisterItemProcessor(RelevantItems.DreadlochEnduranceBooster, RelevantItems.DreadlochEnduranceBooster, EnduranceBooster, CombatActionPriority.High);
+            RegisterItemProcessor(RelevantItems.DreadlochEnduranceBoosterNanomageEdition, RelevantItems.DreadlochEnduranceBoosterNanomageEdition, EnduranceBooster, CombatActionPriority.High);
             // xp stim?
             // health and nano recharger
             // first aid stim
@@ -62,6 +65,7 @@ namespace CombatHandler.Generic
             // special arrows
             // MA attacks
 
+            RegisterSpellProcessor(RelevantNanos.FountainOfLife, FountainOfLife);
 
 
             switch (DynelManager.LocalPlayer.Breed)
@@ -79,6 +83,59 @@ namespace CombatHandler.Generic
                 case Breed.Atrox:
                     break;
             }
+        }
+
+        private bool FountainOfLife(Spell spell, SimpleChar fightingtarget, out SimpleChar target)
+        {
+            // Prioritize keeping ourself alive
+            if (DynelManager.LocalPlayer.HealthPercent <= 30)
+            {
+                target = DynelManager.LocalPlayer;
+                return true;
+            }
+
+            // Try to keep our teammates alive if we're in a team
+            if (DynelManager.LocalPlayer.IsInTeam())
+            {
+                SimpleChar dyingTeamMember = DynelManager.Characters
+                    .Where(c => Team.Members.Select(t => t.Identity.Instance).Contains(c.Identity.Instance))
+                    .Where(c => c.HealthPercent < 30)
+                    .OrderByDescending(c => c.GetStat(Stat.IsFightingMe))
+                    .FirstOrDefault();
+
+                if (dyingTeamMember != null)
+                {
+                    target = dyingTeamMember;
+                    return true;
+                }
+            }
+
+            target = null;
+            return false;
+        }
+
+        private bool EnduranceBooster(Item item, SimpleChar fightingtarget, out SimpleChar target)
+        {
+            target = null;
+
+            // don't use if skill is locked (we will add this dynamically later)
+            if (DynelManager.LocalPlayer.Cooldowns.ContainsKey(Stat.Strength))
+                return false;
+
+            // don't use if we're above 40%
+            if (DynelManager.LocalPlayer.HealthPercent > 40)
+                return false;
+
+            // don't use if nothing is fighting us
+            if (DynelManager.LocalPlayer.GetStat(Stat.IsFightingMe) == 0)
+                return false;
+
+            // don't use if we have another major absorb running
+            // we could check remaining absorb stat to be slightly more effective
+            if (DynelManager.LocalPlayer.Buffs.Contains(Nanoline.BioCocoon))
+                return false;
+
+            return true;
         }
 
         protected virtual bool TargetedDamagePerk(Perk perk, SimpleChar fightingTarget, out SimpleChar target)
@@ -145,6 +202,7 @@ namespace CombatHandler.Generic
                 case RelevantItems.MightOfTheRevenant:
                 case RelevantItems.BarrowStrength:
                     return Stat.Strength;
+                case RelevantItems.MeteoriteSpikes:
                 case RelevantItems.LavaCapsule:
                 case RelevantItems.KizzermoleGumboil:
                     return Stat.SharpObject;
@@ -165,6 +223,14 @@ namespace CombatHandler.Generic
             public const int LavaCapsule = 245990;
             public const int KizzermoleGumboil = 245323;
             public const int SteamingHotCupOfEnhancedCoffee = 157296;
+            public const int DreadlochEnduranceBooster = 267168;
+            public const int DreadlochEnduranceBoosterNanomageEdition = 267167;
+            public const int MeteoriteSpikes = 244204;
+        }
+
+        private static class RelevantNanos
+        {
+            public const int FountainOfLife = 302907;
         }
     }
 }

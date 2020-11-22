0db22470bec89519c200cfaf83eb541cd63c0d77
Tyler Delmus <tylerdelmus@gmail.com>
2020-11-21 21:44:06 -0600

    Added Engi CombatHandler

---
+++ CombatHandler.Engi/Main.cs
@@ -0,0 +1,22 @@
+﻿using AOSharp.Core;
+using System;
+using AOSharp.Core.UI;
+
+namespace CombatHandler.Engi
+{
+    public class Main : IAOPluginEntry
+    {
+        public void Run(string pluginDir)
+        {
+            try
+            {
+                Chat.WriteLine("Engi Combat Handler Loaded!");
+                AOSharp.Core.Combat.CombatHandler.Set(new EngiCombatHandler());
+            }
+            catch (Exception e)
+            {
+                Chat.WriteLine(e.Message);
+            }
+        }
+    }
+}

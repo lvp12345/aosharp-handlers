f01e9f7ae84b1b6bcc43e7b164e73348094339dd
Tyler Delmus <tylerdelmus@gmail.com>
2020-11-22 21:37:35 -0600

    Added pet spawning and buffing to engi combathandler

--- CombatHandler.Engi/EngiCombatHandler.cs
+++ CombatHandler.Engi/EngiCombatHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AOSharp.Common.GameData;
 using AOSharp.Core;
+using AOSharp.Core.Inventory;
 using AOSharp.Core.UI;
 using AOSharp.Core.UI.Options;
 using CombatHandler.Generic;
@@ -11,7 +12,9 @@ namespace CombatHandler.Engi
 {
     class EngiCombatHandler : GenericCombatHandler
     {
+        private const float PostZonePetCheckBuffer = 5;
         private Menu _menu;
+        private double _lastZonedTime = 0;
 
         public EngiCombatHandler()
         {
@@ -46,18 +49,111 @@ namespace CombatHandler.Engi
             RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.ShadowlandReflectBase).OrderByStackingOrder(), GenericBuff);
             RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.SpecialAttackAbsorberBase).OrderByStackingOrder(), GenericBuff);
             RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.EngineerSpecialAttackAbsorber).OrderByStackingOrder(), GenericBuff);
+            RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.ArmorBuff).OrderByStackingOrder(), GenericBuff);
 
+            RegisterSpellProcessor(RelevantNanos.Pets.Where(x => x.Value.PetType == PetType.Attack).Select(x => x.Key).ToArray(), PetSpawner);
+            RegisterSpellProcessor(RelevantNanos.Pets.Where(x => x.Value.PetType == PetType.Support).Select(x => x.Key).ToArray(), PetSpawner);
 
+            RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.PetShortTermDamageBuffs).OrderByStackingOrder(), PetTargetBuff);
+            RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.PetDefensiveNanos).OrderByStackingOrder(), PetTargetBuff);
+            RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.MPPetInitiativeBuffs).OrderByStackingOrder(), PetTargetBuff);
+            RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.EngineerMiniaturization).OrderByStackingOrder(), PetTargetBuff);
+            RegisterSpellProcessor(Spell.GetSpellsForNanoline(Nanoline.ArmorBuff).OrderByStackingOrder(), PetTargetBuff);
+            RegisterSpellProcessor(RelevantNanos.ShieldOfTheObedientServant, ShieldOfTheObedientServant);
 
+            foreach(int shellId in RelevantNanos.Pets.Values.Select(x => x.ShellId))
+                RegisterItemProcessor(shellId, shellId, PetSpawnerItem);
 
             _menu = new Menu("CombatHandler.Engi", "CombatHandler.Engi");
             _menu.AddItem(new MenuBool("BuffPets", "Buff Pets?", true));
-            //Setting this to default to false for now, as I do not currently have the nano ( Will by EOD so expect a patch )
-            _menu.AddItem(new MenuBool("UseSingleTaunt", "Use IMalice?", false));
             OptionPanel.AddMenu(_menu);
+
+            Game.TeleportEnded += OnZoned;
         }
 
+        private void OnZoned(object s, EventArgs e)
+        {
+            _lastZonedTime = Time.NormalTime;
+        }
 
+        protected bool PetSpawner(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
+        {
+            if (Time.NormalTime < _lastZonedTime + PostZonePetCheckBuffer)
+                return false;
+
+            //Do not attempt any pet spawns if we have a pet not loaded as it could be the pet we think we need to replace.
+            if (DynelManager.LocalPlayer.Pets.Any(x => x.Type == PetType.Unknown))
+                return false;
+
+            if (!RelevantNanos.Pets.ContainsKey(spell.Identity.Instance))
+                return false;
+
+            //Ignore spell if we already have this type of pet out
+            if (DynelManager.LocalPlayer.Pets.Any(x => x.Type == RelevantNanos.Pets[spell.Identity.Instance].PetType))
+                return false;
+
+            //Ignore spell if we already have the shell in our inventory
+            if (Inventory.Find(RelevantNanos.Pets[spell.Identity.Instance].ShellId, out Item shell))
+                return false;
+
+            actionTarget.ShouldSetTarget = false;
+            return true;
+        }
+
+        protected virtual bool PetSpawnerItem(Item item, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
+        {
+            if (Time.NormalTime < _lastZonedTime + PostZonePetCheckBuffer)
+                return false;
+
+            if (!RelevantNanos.Pets.Values.Any(x => (x.ShellId == item.LowId || x.ShellId == item.HighId) && !DynelManager.LocalPlayer.Pets.Any(p => p.Type == x.PetType)))
+                return false;
+
+            actionTarget.ShouldSetTarget = false;
+            return true;
+        }
+
+        protected bool PetTargetBuff(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
+        {
+            if (Time.NormalTime < _lastZonedTime + PostZonePetCheckBuffer)
+                return false;
+
+            bool petsNeedBuff = false;
+
+            foreach (Pet pet in DynelManager.LocalPlayer.Pets.Where(x => x.Character != null && (x.Type == PetType.Attack || x.Type == PetType.Support)))
+            {
+                if (pet.Character.Buffs.Find(spell.Nanoline, out Buff buff))
+                {
+                    //Don't cast if weaker than existing
+                    if (spell.StackingOrder < buff.StackingOrder)
+                        continue;
+
+                    //Don't cast if greater than 10% time remaining
+                    if (spell.Nanoline == buff.Nanoline && buff.RemainingTime / buff.TotalTime > 0.1)
+                        continue;
+                }
+
+                actionTarget.Target = pet.Character;
+                petsNeedBuff = true;
+                break;
+            }
+
+            if (!petsNeedBuff)
+                return false;
+
+            actionTarget.ShouldSetTarget = true;
+            return true;
+        }
+
+        protected bool ShieldOfTheObedientServant(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
+        {
+            if (!DynelManager.LocalPlayer.Pets.Where(x => x.Character != null)
+                                            .Where(x => x.Type == PetType.Attack || x.Type == PetType.Support)
+                                            .Any(x => !x.Character.Buffs.Find(spell.Identity.Instance == 270790 ? 285699 : 285698, out _)))
+                return false;
+
+            actionTarget.ShouldSetTarget = false;
+            return true;
+        }
 
         private bool SelfHealPerk(Perk perk, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
         {
@@ -112,8 +208,28 @@ namespace CombatHandler.Engi
             public const int CompositeUtility = 287046;
             public const int CompositeRanged = 223348;
             public const int CompositeRangedSpec = 223364;
-
             public const int SympatheticReactiveCocoon = 154550;
+            public static int[] ShieldOfTheObedientServant = { 270790, 202260 };
+
+            public static Dictionary<int, PetSpellData> Pets = new Dictionary<int, PetSpellData>
+            { 
+                { 223323, new PetSpellData(217994, PetType.Attack) },
+                { 275815, new PetSpellData(275816, PetType.Support) }
+            };
+
+            //Doggos 275815, 223337, 223335, 223333, 223331, 223329, 223327, 301855, 223325 
+        }
+
+        private class PetSpellData
+        {
+            public int ShellId;
+            public PetType PetType;
+
+            public PetSpellData(int shellId, PetType petType)
+            {
+                ShellId = shellId;
+                PetType = petType;
+            }
         }
     }
 }

3b52c8f706f0252f6cff319d51b0fd63a44f0730
John Ralph <genysisgamingstream@gmail.com>
2021-07-24 18:36:13 +0100

    Z Updates - Added OS Healing to doc, cleaned up nanos

--- CombatHandler.Doc/DocCombatHandler.cs
+++ CombatHandler.Doc/DocCombatHandler.cs
@@ -19,8 +19,8 @@ namespace Desu
             settings.AddVariable("DotC", false);
             settings.AddVariable("CH", true);
             settings.AddVariable("IndividualHOT", false);
-            settings.AddVariable("SLHeal", true);
-            settings.AddVariable("RKHeal", false);
+            settings.AddVariable("Heal", true);
+            settings.AddVariable("OSHeal", false);
             settings.AddVariable("ShortHPBuff", false);
             settings.AddVariable("LockCH", false);
             RegisterSettingsWindow("Doctor Handler", "DoctorSettingsView.xml");
@@ -33,8 +33,8 @@ namespace Desu
 
             //Spells
             RegisterSpellProcessor(Spell.GetSpellsForNanoline(NanoLine.CompleteHealingLine).OrderByStackingOrder(), CompleteHealing, CombatActionPriority.High);
-            RegisterSpellProcessor(RelevantNanos.SL_HEALS, ShadowlandsHeal, CombatActionPriority.Medium);
-            RegisterSpellProcessor(RelevantNanos.RK_HEALS, RubiKaHeal, CombatActionPriority.Medium);
+            RegisterSpellProcessor(RelevantNanos.HEALS, Heals, CombatActionPriority.Medium);
+            //RegisterSpellProcessor(RelevantNanos.RK_HEALS, RubiKaHeal, CombatActionPriority.Medium);
             RegisterSpellProcessor(RelevantNanos.IMPROVED_LC, TeamHeal, CombatActionPriority.High);
 
             //Team Buffs
@@ -212,7 +212,7 @@ namespace Desu
 
         private bool TeamHeal(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
         {
-            if (!IsSettingEnabled("SLHeal"))
+            if (!IsSettingEnabled("Heal"))
             {
                 return false;
             }
@@ -236,24 +236,32 @@ namespace Desu
             return false;
         }
 
-        private bool ShadowlandsHeal(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
+        private bool Heals(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
         {
-            if(!IsSettingEnabled("SLHeal"))
+            if(!IsSettingEnabled("Heal") && !IsSettingEnabled("OSHeal"))
             {
                 return false;
             }
 
-            return FindMemberWithHealthBelow(90, ref actionTarget);
-        }
-
-        private bool RubiKaHeal(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
-        {
-            if (!IsSettingEnabled("RKHeal"))
+            if (IsSettingEnabled("Heal") && IsSettingEnabled("OSHeal"))
             {
                 return false;
             }
-            return FindMemberWithHealthBelow(90, ref actionTarget);
+
+            if (IsSettingEnabled("OSHeal"))
+                return FindPlayerWithHealthBelow(90, ref actionTarget);
+            else
+                return FindMemberWithHealthBelow(90, ref actionTarget);
         }
+
+        //private bool RubiKaHeal(Spell spell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
+        //{
+        //    if (!IsSettingEnabled("RKHeal"))
+        //    {
+        //        return false;
+        //    }
+        //    return FindMemberWithHealthBelow(90, ref actionTarget);
+        //}
 
         private bool FindMemberWithHealthBelow(int healthPercentTreshold, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
         {
@@ -283,6 +291,31 @@ namespace Desu
             return false;
         }
 
+        private bool FindPlayerWithHealthBelow(int healthPercentTreshold, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
+        {
+            // Prioritize keeping ourself alive
+            if (DynelManager.LocalPlayer.HealthPercent <= healthPercentTreshold)
+            {
+                actionTarget.Target = DynelManager.LocalPlayer;
+                return true;
+            }
+
+            // Try to keep our teammates alive if we're in a team
+            SimpleChar dyingTeamMember = DynelManager.Characters
+                .Where(c => c.HealthPercent <= healthPercentTreshold)
+                .OrderByDescending(c => c.GetStat(Stat.NumFightingOpponents))
+                .FirstOrDefault();
+
+            if (dyingTeamMember != null)
+            {
+                actionTarget.Target = dyingTeamMember;
+                actionTarget.ShouldSetTarget = true;
+                return true;
+            }
+
+            return false;
+        }
+
         private bool HasNano(int nanoId)
         {
             return Spell.Find(nanoId, out Spell spell);
@@ -306,12 +339,15 @@ namespace Desu
             //HP buffs don't have a nano line and cannot be ordered dynamically. Need to hardcode list and order.
             public static int[] HP_BUFFS = new[] { 95709, 28662, 95720, 95712, 95710, 95711, 28649, 95713, 28660, 95715, 95714, 95718, 95716, 95717, 95719, 42397 };
             //RK Heals don't have a nano line and cannot be ordered dynamically. Need to hardcode list and order.
-            public static int[] RK_HEALS = new[] 
-            { 43885, 43887, 43890, 43884 , 43808 , 43888 , 43889 ,43883, 43811, 43809, 43810, 28645, 43816, 43817, 43825, 43815, 
-                43814, 43821, 43820, 28648, 43812, 43824, 43822, 43819, 43818, 43823, 28677, 43813, 43826, 43838, 43835, 
-                28672, 43836, 28676, 43827, 43834, 28681, 43837, 43833, 43830, 43828, 28654, 43831, 43829, 43832, 28665 };
+            //public static int[] RK_HEALS = new[] 
+            //{ 43885, 43887, 43890, 43884 , 43808 , 43888 , 43889 ,43883, 43811, 43809, 43810, 28645, 43816, 43817, 43825, 43815, 
+            //    43814, 43821, 43820, 28648, 43812, 43824, 43822, 43819, 43818, 43823, 28677, 43813, 43826, 43838, 43835, 
+            //    28672, 43836, 28676, 43827, 43834, 28681, 43837, 43833, 43830, 43828, 28654, 43831, 43829, 43832, 28665 };
             //SL Heals have a broken stacking order and cannot be ordered dynamically. Need to hardcode order.
-            public static int[] SL_HEALS = new[] { 223299, 223297, 223295, 223293, 223291, 223289, 223287, 223285, 223281, 43878, 43881, 43886 };
+            public static int[] HEALS = new[] { 223299, 223297, 223295, 223293, 223291, 223289, 223287, 223285, 223281, 43878, 43881, 43886, 43885, 
+                43887, 43890, 43884, 43808, 43888, 43889, 43883, 43811, 43809, 43810, 28645, 43816, 43817, 43825, 43815,
+                43814, 43821, 43820, 28648, 43812, 43824, 43822, 43819, 43818, 43823, 28677, 43813, 43826, 43838, 43835,
+                28672, 43836, 28676, 43827, 43834, 28681, 43837, 43833, 43830, 43828, 28654, 43831, 43829, 43832, 28665 };
         }
     }
 }

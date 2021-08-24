7169fd907bc0583b0c56792bb801283cc2a98ff1
John Ralph <genysisgamingstream@gmail.com>
2021-08-24 23:35:24 +0100

    Z Updates - Added better checking of primary and secondary weapons, pull latest aosharp

--- CombatHandler.Generic/CharacterWieldedWeapon.cs
+++ CombatHandler.Generic/CharacterWieldedWeapon.cs
@@ -6,30 +6,29 @@ using System.Threading.Tasks;
 
 namespace MultiboxHelper
 {
+
+    [Flags]
     public enum CharacterWieldedWeapon
     {
-        Bandaid = 1337,
-        Invalid = 0,
-        Fists = 1,
-        Bow = 12,
-        Smg = 20,
-        Edged1H = 34,
-        Blunt1H = 66,
-        Edged2H = 130,
-        Blunt2H = 258,
-        Piercing = 514,
-        Pistol = 1028,
-        AssaultRifle = 2052,
-        Rifle = 4100,
-        Shotgun = 8196,
-        Grenade = 32772,
-        PistolAndShotgun = 9220,
-        HeavyWeapons = 65540,
-        PistolAndAssaultRifle = 3076,
-        Blunt1HAndEnergy = 16450,
-        Energy = 16386,
-        Blunt1HAndEdged1H = 98,
-        Blunt1HAndPiercing = 578,
-        Edged1HAndPiercing = 546,
+        Invalid = 0x0,            // 0x00000000000000000000b
+        Fists = 0x01,             // 0x00000000000000000001b
+        Melee = 0x02,             // 0x00000000000000000010b
+        Ranged = 0x04,            // 0x00000000000000000100b
+        Bow = 0x08,               // 0x00000000000000001000b
+        Smg = 0x10,               // 0x00000000000000010000b
+        Edged1H = 0x20,           // 0x00000000000000100000b
+        Blunt1H = 0x40,           // 0x00000000000001000000b
+        Edged2H = 0x80,           // 0x00000000000010000000b
+        Blunt2H = 0x100,          // 0x00000000000100000000b
+        Piercing = 0x200,         // 0x00000000001000000000b
+        Pistol = 0x400,           // 0x00000000010000000000b
+        AssaultRifle = 0x800,     // 0x00000000100000000000b
+        Rifle = 0x1000,           // 0x00000001000000000000b
+        Shotgun = 0x2000,         // 0x00000010000000000000b
+        MartialArts = 0x4000,     // 0x00000100000000000000b
+        MeleeEnergy = 0x8000,     // 0x00001000000000000000b 0x100000000000010b
+        RangedEnergy = 0x10000,   // 0x00010000000000000000b
+        Grenade = 0x20000,        // 0x00100000000000000000b
+        HeavyWeapons = 0x40000,   // 0x01000000000000000000b
     }
 }

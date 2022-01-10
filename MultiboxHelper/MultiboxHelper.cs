a4cf621703c4f12f721cb73b618652d8876130d5
John Ralph <genysisgamingstream@gmail.com>
2022-01-10 10:56:34 +0000

    Z Updates - Fixes

--- MultiboxHelper/MultiboxHelper.cs
+++ MultiboxHelper/MultiboxHelper.cs
@@ -798,7 +798,7 @@ namespace MultiboxHelper
                     && !Team.IsInCombat && DynelManager.LocalPlayer.FightingTarget == null
                     && !DynelManager.LocalPlayer.IsMoving && !Game.IsZoning && Time.NormalTime > usedSitPetUpdateTimer + 16)
                 {
-                    if (healpet.Character.Nano / PetMaxNanoPool() * 100 > 55) { return; }
+                    if (PetMaxNanoPool() / healpet.Character.Nano * 100 > 55) { return; }
 
                     MovementController.Instance.SetMovement(MovementAction.SwitchToSit);
 

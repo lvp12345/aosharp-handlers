064344afcd626f226f1147bfeea8dbf0c977841a
John Ralph <genysisgamingstream@gmail.com>
2022-05-26 05:54:38 +0100

    Z Updates - Added Research

---
+++ Research/Research.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Diagnostics;
+using AOSharp.Core;
+using AOSharp.Core.IPC;
+using AOSharp.Core.UI;
+using AOSharp.Common.GameData;
+using SmokeLounge.AOtomation.Messaging.Messages;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using AOSharp.Core.Inventory;
+using AOSharp.Common.Unmanaged.Interfaces;
+using System.Linq;
+
+namespace Research
+{
+    public class Research : AOPluginEntry
+    {
+        public static string PluginDirectory;
+
+        private static Settings _settings = new Settings("Research");
+
+        public static List<ResearchGoal> _researchGoals;
+        public static List<int> _researchGoalsActive = new List<int>();
+        public static List<uint> _completedResearchGoals = new List<uint>();
+
+        public int _goal = 0;
+
+        public override void Run(string pluginDir)
+        {
+            PluginDirectory = pluginDir;
+
+            SettingsController.RegisterSettingsWindow("Research", pluginDir + $"\\UI\\Research{DynelManager.LocalPlayer.Profession}View.xml", _settings);
+
+            _settings.AddVariable("Toggle", false);
+
+            _settings.AddVariable("Line1", false);
+            _settings.AddVariable("Line2", false);
+            _settings.AddVariable("Line3", false);
+            _settings.AddVariable("Line4", false);
+            _settings.AddVariable("Line5", false);
+            _settings.AddVariable("Line6", false);
+            _settings.AddVariable("Line7", false);
+            _settings.AddVariable("Line8", false);
+
+            //Chat.RegisterCommand("test", (string command, string[] param, ChatWindow chatWindow) =>
+            //{
+            //    if (_goal < N3EngineClientAnarchy.GetPersonalResearchGoals().Count)
+            //    {
+            //        _goal++;
+
+            //        Perk _research = Perk.GetByInstance(_researchGoals[_goal].ResearchId);
+
+            //        PerkLine _perkLine = _research.PerkLine;
+
+            //        Chat.WriteLine($"Next - level {Perk.GetPerkLineLevel(_perkLine)}, perkline {_perkLine} ");
+
+            //        DynelManager.LocalPlayer.SetStat(Stat.PersonalResearchGoal, _researchGoals[_goal].ResearchId);
+            //    }
+            //    else if (_goal == N3EngineClientAnarchy.GetPersonalResearchGoals().Count)
+            //    {
+            //        _goal = 0;
+
+            //        DynelManager.LocalPlayer.SetStat(Stat.PersonalResearchGoal, _researchGoals[_goal].ResearchId);
+
+            //        Perk _research = Perk.GetByInstance(_researchGoals[_goal].ResearchId);
+
+            //        PerkLine _perkLine = _research.PerkLine;
+
+            //        Chat.WriteLine($"Last - level {Perk.GetPerkLineLevel(_perkLine)}, perkline {_perkLine} ");
+            //    }
+            //});
+
+            Game.OnUpdate += OnUpdate;
+
+
+            Chat.WriteLine("Research Loaded!");
+            Chat.WriteLine("/research for settings.");
+        }
+
+        public override void Teardown()
+        {
+            SettingsController.CleanUp();
+        }
+
+        private void OnUpdate(object s, float deltaTime)
+        {
+            if (_settings["Toggle"].AsBool() && !Game.IsZoning)
+            {
+                _researchGoals = N3EngineClientAnarchy.GetPersonalResearchGoals()
+                    .Where(c => c.Available == true)
+                    .ToList();
+
+                _completedResearchGoals = N3EngineClientAnarchy.GetCompletedPersonalResearchGoals();
+
+                InitRemoveResearch();
+                InitAddResearch();
+
+                if (_completedResearchGoals.Contains((uint)DynelManager.LocalPlayer.GetStat(Stat.PersonalResearchGoal)))
+                {
+                    Chat.WriteLine($"Research line finished - {N3EngineClientAnarchy.GetPerkName(_researchGoals[_goal].ResearchId)}");
+                    _researchGoalsActive.Remove(_goal + 1);
+
+                    _goal++;
+                    DynelManager.LocalPlayer.SetStat(Stat.PersonalResearchGoal, _researchGoals[_goal].ResearchId);
+                }
+            }
+        }
+
+        public static void InitAddResearch()
+        {
+            if (_settings["Line1"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(1))
+                    _researchGoalsActive.Add(1);
+            }
+            if (_settings["Line2"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(2))
+                    _researchGoalsActive.Add(2);
+            }
+            if (_settings["Line3"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(3))
+                    _researchGoalsActive.Add(3);
+            }
+            if (_settings["Line4"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(4))
+                    _researchGoalsActive.Add(4);
+            }
+            if (_settings["Line5"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(5))
+                    _researchGoalsActive.Add(5);
+            }
+            if (_settings["Line6"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(6))
+                    _researchGoalsActive.Add(6);
+            }
+            if (_settings["Line7"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(7))
+                    _researchGoalsActive.Add(7);
+            }
+            if (_settings["Line8"].AsBool())
+            {
+                if (!_researchGoalsActive.Contains(8))
+                    _researchGoalsActive.Add(8);
+            }
+        }
+
+        public static void InitRemoveResearch()
+        {
+            if (!_settings["Line1"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(1))
+                    _researchGoalsActive.Remove(1);
+            }
+            if (!_settings["Line2"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(2))
+                    _researchGoalsActive.Remove(2);
+            }
+            if (!_settings["Line3"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(3))
+                    _researchGoalsActive.Remove(3);
+            }
+            if (!_settings["Line4"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(4))
+                    _researchGoalsActive.Remove(4);
+            }
+            if (!_settings["Line5"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(5))
+                    _researchGoalsActive.Remove(5);
+            }
+            if (!_settings["Line6"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(6))
+                    _researchGoalsActive.Remove(6);
+            }
+            if (!_settings["Line7"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(7))
+                    _researchGoalsActive.Remove(7);
+            }
+            if (!_settings["Line8"].AsBool())
+            {
+                if (_researchGoalsActive.Contains(8))
+                    _researchGoalsActive.Remove(8);
+            }
+        }
+    }
+}
---
+++ Research/SettingsController.cs
@@ -0,0 +1,84 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Core.UI;
+using AOSharp.Core;
+using System;
+using System.Collections.Generic;
+using AOSharp.Common.GameData.UI;
+
+namespace Research
+{
+    public static class SettingsController
+    {
+        private static List<Settings> settingsToSave = new List<Settings>();
+        public static Dictionary<string, string> settingsWindows = new Dictionary<string, string>();
+        private static bool IsCommandRegistered;
+
+        public static Window settingsWindow;
+
+        public static void RegisterCharacters(Settings settings)
+        {
+            RegisterChatCommandIfNotRegistered();
+            settingsToSave.Add(settings);
+        }
+
+        public static void RegisterSettingsWindow(string settingsName, string settingsWindowPath, Settings settings)
+        {
+            RegisterChatCommandIfNotRegistered();
+            settingsWindows[settingsName] = settingsWindowPath;
+            settingsToSave.Add(settings);
+        }
+
+        public static void RegisterSettings(Settings settings)
+        {
+            RegisterChatCommandIfNotRegistered();
+            settingsToSave.Add(settings);
+        }
+
+        public static void CleanUp()
+        {
+            settingsToSave.ForEach(settings => settings.Save());
+        }
+
+        private static void RegisterChatCommandIfNotRegistered()
+        {
+            if (!IsCommandRegistered)
+            {
+                Chat.RegisterCommand("infbuddy", (string command, string[] param, ChatWindow chatWindow) =>
+                {
+                    try
+                    {
+                        settingsWindow = Window.Create(new Rect(50, 50, 220, 300), "Research", "Settings", WindowStyle.Default, WindowFlags.None);
+
+                        if (settingsWindow != null && !settingsWindow.IsVisible)
+                        {
+                            AppendSettingsTab("Research", settingsWindow);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Chat.WriteLine(e);
+                    }
+                });
+
+                IsCommandRegistered = true;
+            }
+        }
+
+        public static void AppendSettingsTab(String settingsName, Window testWindow)
+        {
+            String settingsWindowXmlPath = settingsWindows[settingsName];
+            View settingsView = View.CreateFromXml(settingsWindowXmlPath);
+
+            if (settingsView != null)
+            {
+                testWindow.AppendTab(settingsName, settingsView);
+                testWindow.Show(true);
+            }
+            else
+            {
+                Chat.WriteLine($"{settingsWindows[settingsName]}");
+                Chat.WriteLine("Failed to load settings schema from " + settingsWindowXmlPath);
+            }
+        }
+    }
+}

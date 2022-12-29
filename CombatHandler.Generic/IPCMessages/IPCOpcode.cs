3e237eb95c556ba8bbf712e9792d44549797f300
Genysis Gaming <kiecall45@aol.com>
2022-12-29 19:32:21 +0000

    Z Updates - woops

--- CombatHandler.Generic/IPCMessages/IPCOpcode.cs
+++ CombatHandler.Generic/IPCMessages/IPCOpcode.cs
@@ -8,9 +8,6 @@ namespace CombatHandler.Generic
 {
     public enum IPCOpcode
     {
-        RemainingNCU = 2000,
-        GlobalBuffing = 2001,
-        GlobalComposites = 2002,
-        GlobalDebuffing = 2003
+        Disband = 2100,
     }
 }

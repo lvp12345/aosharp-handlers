049c27ea773adbf3621cf7be0bbf0bfa89cfb67c
Genysis Gaming <kiecall45@aol.com>
2022-12-30 00:16:35 +0000

    Z Updates - agent oopsy

--- CombatHandler.Agent/AgentCombatHandler.cs
+++ CombatHandler.Agent/AgentCombatHandler.cs
@@ -382,6 +382,87 @@ namespace CombatHandler.Agent
                     falseProfView.Tag = SettingsController.settingsWindow;
                     falseProfView.Clicked = HandleFalseProfViewClick;
                 }
+
+                #region GlobalBuffing
+
+                if (!_settings["GlobalBuffing"].AsBool() && ToggleBuffing)
+                {
+                    IPCChannel.Broadcast(new GlobalBuffingMessage()
+                    {
+                        Switch = false
+                    });
+
+                    ToggleBuffing = false;
+                    _settings["Buffing"] = false;
+                    _settings["GlobalBuffing"] = false;
+                }
+
+                if (_settings["GlobalBuffing"].AsBool() && !ToggleBuffing)
+                {
+                    IPCChannel.Broadcast(new GlobalBuffingMessage()
+                    {
+                        Switch = true
+                    });
+
+                    ToggleBuffing = true;
+                    _settings["Buffing"] = true;
+                    _settings["GlobalBuffing"] = true;
+                }
+
+                #endregion
+
+                #region Composites
+
+                if (!_settings["GlobalComposites"].AsBool() && ToggleComposites)
+                {
+                    IPCChannel.Broadcast(new GlobalCompositesMessage()
+                    {
+                        Switch = false
+                    });
+
+                    ToggleComposites = false;
+                    _settings["Composites"] = false;
+                    _settings["GlobalComposites"] = false;
+                }
+                if (_settings["GlobalComposites"].AsBool() && !ToggleComposites)
+                {
+                    IPCChannel.Broadcast(new GlobalCompositesMessage()
+                    {
+                        Switch = true
+                    });
+
+                    ToggleComposites = true;
+                    _settings["Composites"] = true;
+                    _settings["GlobalComposites"] = true;
+                }
+
+                #endregion
+
+                #region Debuffing
+
+                //if (!_settings["GlobalDebuffing"].AsBool() && ToggleDebuffing)
+                //{
+                //    IPCChannel.Broadcast(new GlobalDebuffingMessage()
+                //    {
+
+                //        Switch = false
+                //    });
+
+                //    ToggleDebuffing = false;
+                //    _settings["GlobalDebuffing"] = false;
+                //}
+                //if (_settings["GlobalDebuffing"].AsBool() && !ToggleDebuffing)
+                //{
+                //    IPCChannel.Broadcast(new GlobalDebuffingMessage()
+                //    {
+                //        Switch = true
+                //    });
+
+                //    ToggleDebuffing = true;
+                //    _settings["GlobalDebuffing"] = true;
+                //}
+
+                #endregion
             }
 
             if (CanLookupPetsAfterZone() && DynelManager.LocalPlayer.Pets.Count() >= 1)

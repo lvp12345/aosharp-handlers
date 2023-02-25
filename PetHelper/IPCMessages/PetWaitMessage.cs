7539a9c4b648216b8efb49b04efe17fd9f65784f
Teknologist <mike@paradisesgarage.com>
2023-02-25 17:09:58 -0500

    Added IPCOpcode for remote Pet commands. Fixed other issues to make the 3 pet commands work from any prof and with Social pets too.

---
+++ PetHelper/IPCMessages/PetWaitMessage.cs
@@ -0,0 +1,11 @@
+﻿using AOSharp.Core.IPC;
+using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
+
+namespace PetHelper.IPCMessages
+{
+    [AoContract((int)IPCOpcode.PetWait)]
+    public class PetWaitMessage : IPCMessage
+    {
+        public override short Opcode => (short)IPCOpcode.PetWait;
+    }
+}

5ebe950c9f71745bfe53451fd17659e53317b319
Knowidea <Mellowclads@hotmail.com>
2023-03-15 18:43:37 -0500

    Added WarpManager still workling on it but is working

---
+++ WarpManager/Config.cs
@@ -0,0 +1,13 @@
+﻿using AOSharp.Common.GameData;
+
+namespace WarpManager.Models
+{
+
+    public class Config
+    {
+        //logging
+        public static LogLevel LOG_LEVEL = LogLevel.DEBUG;
+        public static LogStyle LOG_STYLE = LogStyle.QUIET;
+
+    }
+}

f447098a44cbf4c378f0a73d97afade38e8407bb
Knowidea <Mellowclads@hotmail.com>
2023-03-25 23:35:05 -0500

    changed doc and nt items again

--- CombatHandler.Doc/DocCombatHandler.cs
+++ CombatHandler.Doc/DocCombatHandler.cs
@@ -1194,7 +1194,8 @@ namespace CombatHandler.Doctor
 
         private bool TOTWHeal(Item item, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
         {
-            if (!IsSettingEnabled("TOTWBooks") && fightingTarget == null || DynelManager.LocalPlayer.Cooldowns.ContainsKey(Stat.BiologicalMetamorphosis)) { return false; }
+            if (!IsSettingEnabled("TOTWBooks") && fightingTarget == null ) { return false; }
+            if (DynelManager.LocalPlayer.Cooldowns.ContainsKey(Stat.BiologicalMetamorphosis)) { return false; }
 
             if (Team.IsInTeam)
             {
---
+++ WarpManager/MainWindowComponent.cs
@@ -0,0 +1,72 @@
+﻿using AOSharp.Core;
+using AOSharp.Core.Inventory;
+using AOSharp.Core.UI;
+using System.Collections.Generic;
+
+
+namespace WarpManager
+{
+    public class MainWindowComponent
+    {
+        
+        private static WarpLogger logger = WarpLogger.GetLogger("MainWindowService");
+        private static MainWindowComponent instance = null;
+
+        
+
+        private MainWindowComponent() 
+        {
+            logger.debug($"MainWindowComponent initialized!");
+        }
+
+        public static MainWindowComponent CreateInstance() 
+        {
+            if (instance == null)
+                instance = new MainWindowComponent();
+
+            return instance;
+        }
+
+        public static void DestroyInstance()
+        {
+            logger.info($"Destroying MainWindowComponent Services...");
+            if (instance != null)
+                instance = null;
+        }
+
+        public static void Render() 
+        {
+            SettingsController.settingsWindow.Show(true);
+            if (SettingsController.settingsWindow.IsValid)
+            {
+
+                if (SettingsController.settingsWindow.FindView("warpForwardButton", out ButtonBase warpForwardButton))
+                {
+                    warpForwardButton.Clicked += WarpManagerComponent.WarpForwardButtonCallBack;
+                }
+
+                //if (SettingsController.settingsWindow.FindView("warpToTargetButton", out ButtonBase warpToTargetButton))
+                //{
+                //    warpToTargetButton.Clicked += WarpManagerComponent.WarpToTargetButtonCallBack;
+                //}
+            }
+        }
+        public static void Updates()
+        {
+            if (SettingsController.settingsWindow.IsValid)
+            {
+                
+                
+                if (SettingsController.settingsWindow.FindView("infoLabel", out TextView infoLabel))
+                    infoLabel.Text = $"Information:";
+
+                if (SettingsController.settingsWindow.FindView("versionLabel", out TextView versionLabel))
+                    versionLabel.Text = $"WarpManager v1.0.0";
+
+            }
+        }
+
+
+        
+    }
+}

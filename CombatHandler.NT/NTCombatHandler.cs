5a488f18c9949c27d1cb1cfd71104c507575c666
Knowidea <Mellowclads@hotmail.com>
2023-03-26 14:01:45 -0500

    Fixed some more items

--- CombatHandler.NT/NTCombatHandler.cs
+++ CombatHandler.NT/NTCombatHandler.cs
@@ -1047,6 +1047,7 @@ namespace CombatHandler.NanoTechnician
         private bool NotumItem(Item item, SimpleChar fightingtarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
         {
             if (!IsSettingEnabled("NotumGrafttSelection")) { return false; }
+            if (Item.HasPendingUse) { return false; }
             if (DynelManager.LocalPlayer.Cooldowns.ContainsKey(Stat.MaxNanoEnergy)) { return false; }
             
 
@@ -1059,7 +1060,7 @@ namespace CombatHandler.NanoTechnician
         private bool Illusionist(Item item, SimpleChar fightingtarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget)
         {
             if (!IsSettingEnabled("IllusionistSelection") && DynelManager.LocalPlayer.Buffs.Contains(274736)) { return false; }
-
+            if (Item.HasPendingUse) { return false; }
             if (fightingtarget?.MaxHealth > 1000000) { return true; }
 
             return false;

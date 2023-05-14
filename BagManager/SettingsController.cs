2ae18ad4c73c424d16b13ac728fe25ea7e406de1
Knowidea <Mellowclads@hotmail.com>
2023-05-14 07:09:04 -0500

    added ZZZs bagmanager

---
+++ BagManager/SettingsController.cs
@@ -0,0 +1,96 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Core.UI;
+using AOSharp.Core;
+using System;
+using System.Collections.Generic;
+using AOSharp.Common.GameData.UI;
+
+namespace BagManager
+{
+    public class WindowOptions
+    {
+        public string Name { get; set; }
+        public string XmlViewName { get; set; }
+        public Rect WindowSize { get; set; } = new Rect(0, 0, 240, 345);
+        public WindowStyle Style { get; set; } = WindowStyle.Default;
+        public WindowFlags Flags { get; set; } = WindowFlags.AutoScale | WindowFlags.NoFade;
+    }
+
+    public static class SettingsController
+    {
+        private static List<Settings> settingsToSave = new List<Settings>();
+        public static Dictionary<string, string> settingsWindows = new Dictionary<string, string>();
+        private static bool IsCommandRegistered;
+
+        public static string _staticName = string.Empty;
+
+        public static Window settingsWindow;
+        public static View settingsView;
+
+        public static void RegisterCharacters(Settings settings)
+        {
+            RegisterChatCommandIfNotRegistered();
+            settingsToSave.Add(settings);
+        }
+
+        public static void RegisterSettingsWindow(string settingsName, string settingsWindowPath, Settings settings)
+        {
+            RegisterChatCommandIfNotRegistered();
+            settingsWindows[settingsName] = settingsWindowPath;
+            settingsToSave.Add(settings);
+        }
+
+        public static void RegisterSettings(Settings settings)
+        {
+            RegisterChatCommandIfNotRegistered();
+            settingsToSave.Add(settings);
+        }
+
+        public static void CleanUp()
+        {
+            settingsToSave.ForEach(settings => settings.Save());
+        }
+
+        private static void RegisterChatCommandIfNotRegistered()
+        {
+            if (!IsCommandRegistered)
+            {
+                Chat.RegisterCommand("bagmanager", (string command, string[] param, ChatWindow chatWindow) =>
+                {
+                    try
+                    {
+                        settingsWindow = Window.Create(new Rect(50, 50, 300, 300), "Bag Manager", "Settings", WindowStyle.Default, WindowFlags.AutoScale);
+
+                        if (settingsWindow != null && !settingsWindow.IsVisible)
+                        {
+                            AppendSettingsTab("Bag Manager", settingsWindow);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Chat.WriteLine(e);
+                    }
+                });
+
+                IsCommandRegistered = true;
+            }
+        }
+
+        public static void AppendSettingsTab(String settingsName, Window testWindow)
+        {
+            String settingsWindowXmlPath = settingsWindows[settingsName];
+            View settingsView = View.CreateFromXml(settingsWindowXmlPath);
+
+            if (settingsView != null)
+            {
+                testWindow.AppendTab(settingsName, settingsView);
+                testWindow.Show(true);
+            }
+            else
+            {
+                Chat.WriteLine($"{settingsWindows[settingsName]}");
+                Chat.WriteLine("Failed to load settings schema from " + settingsWindowXmlPath);
+            }
+        }
+    }
+}

de987f3201f91d52ecfcfb3eb386896ddbd9af69
Knowidea <mellowclads@hotmail.com>
2023-10-04 09:46:20 -0500

    Advy: GenericSelectionBuff and UI region

--- CombatHandler.Crat/CratCombatHandler.cs
+++ CombatHandler.Crat/CratCombatHandler.cs
@@ -200,7 +200,8 @@ namespace CombatHandler.Bureaucrat
 
             //Team Buffs
             RegisterSpellProcessor(Spell.GetSpellsForNanoline(NanoLine.InitiativeBuffs).OrderByStackingOrder(),
-                (Spell buffSpell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget) => GenericSelectionBuff(buffSpell, fightingTarget, ref actionTarget, "InitBuffSelection"));
+                (Spell buffSpell, SimpleChar fightingTarget, ref (SimpleChar Target, bool ShouldSetTarget) actionTarget) 
+                => GenericSelectionBuff(buffSpell, fightingTarget, ref actionTarget, "InitBuffSelection"));
 
 
             RegisterSpellProcessor(Spell.GetSpellsForNanoline(NanoLine.NanoDeltaBuffs).OrderByStackingOrder(), NanoDelta);

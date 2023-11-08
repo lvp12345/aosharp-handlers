6c243435f18257eef8fb2bd4eeeac147ac40ac54
Knowidea <mellowclads@hotmail.com>
2023-11-08 14:53:54 -0600

    Sync: refactored sync chat and reorganized

--- SyncManager/IPCMessages/IPCOpcode.cs
+++ SyncManager/IPCMessages/IPCOpcode.cs
@@ -3,13 +3,10 @@
     public enum IPCOpcode
     {
         Move = 200,
-        Jump = 201,
         Target = 202,
         Attack = 203,
         Use = 205,
-        NpcChatOpen = 206,
-        NpcChatClose = 207,
-        NpcChatAnswer = 208,
+        NpcChat = 206,
         UseItem = 209,
         Trade = 210,
         StartStop = 211
---
+++ SyncManager/IPCMessages/NpcChatMessage.cs
@@ -0,0 +1,21 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Core.IPC;
+using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
+
+namespace SyncManager.IPCMessages
+{
+    [AoContract((int)IPCOpcode.NpcChat)]
+    public class NpcChatIPCMessage : IPCMessage
+    {
+        public override short Opcode => (short)IPCOpcode.NpcChat;
+
+        [AoMember(0)]
+        public Identity Target { get; set; }
+
+        [AoMember(1)]
+        public bool? OpenClose { get; set; } 
+
+        [AoMember(2)]
+        public int? Answer { get; set; } 
+    }
+}

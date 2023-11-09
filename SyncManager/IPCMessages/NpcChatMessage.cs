b9e0d45c49853deaef71b7d479d51a51f8cba0f3
Knowidea <Mellowclads@hotmail.com>
2023-11-08 20:13:33 -0600

    Sync: more worked . sync chat now will use credits

--- SyncManager/IPCMessages/NpcChatMessage.cs
+++ SyncManager/IPCMessages/NpcChatMessage.cs
@@ -1,5 +1,6 @@
 ﻿using AOSharp.Common.GameData;
 using AOSharp.Core.IPC;
+using SmokeLounge.AOtomation.Messaging.GameData;
 using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
 
 namespace SyncManager.IPCMessages
@@ -13,9 +14,25 @@ namespace SyncManager.IPCMessages
         public Identity Target { get; set; }
 
         [AoMember(1)]
-        public bool? OpenClose { get; set; } 
+        public bool OpenClose { get; set; }
 
         [AoMember(2)]
-        public int? Answer { get; set; } 
+        public int Answer { get; set; }
+
+        [AoMember(3)]
+        public bool IsStartTrade { get; set; } 
+        [AoMember(4)]
+        public bool IsTrade { get; set; } 
+        [AoMember(5)]
+        public bool IsFinishTrade { get; set; } 
+        [AoMember(6)]
+        public Identity Container { get; set; } 
+        [AoMember(7)]
+        public int NumberOfItemSlotsInTradeWindow { get; set; } 
+        [AoMember(8)]
+        public int Decline { get; set; } 
+        [AoMember(9)]
+        public int Amount { get; set; }
+
     }
 }

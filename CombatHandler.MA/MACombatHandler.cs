4ed8f2017645f8c087366ddd864941226b52ff75
Knowidea <Mellowclads@hotmail.com>
2023-12-15 17:33:44 -0600

    still working on adding to reload before fa and burst

--- CombatHandler.MA/MACombatHandler.cs
+++ CombatHandler.MA/MACombatHandler.cs
@@ -969,11 +969,6 @@ namespace CombatHandler.MartialArtist
 
         #region Misc
 
-        protected override bool ShouldUseSpecialAttack(SpecialAttack specialAttack)
-        {
-            return specialAttack != SpecialAttack.Dimach;
-        }
-
         private static class RelevantNanos
         {
             public const int FistsOfTheWinterFlame = 269470;

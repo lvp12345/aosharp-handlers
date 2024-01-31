dd238d9780375016446fc20f2462f3b88962bf95
Knowidea <mellowclads@hotmail.com>
2024-01-31 16:33:59 +0000

    added eumenides to helpmanager

---
+++ HelpManager/IPCMessages/UISettings.cs
@@ -0,0 +1,22 @@
+﻿using AOSharp.Core.IPC;
+using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
+
+namespace HelpManager.IPCMessages
+{
+    [AoContract((int)IPCOpcode.UISettings)]
+    internal class UISettings : IPCMessage
+    {
+        [AoMember(0)]
+        public bool BroadcastSettings { get; set; }
+        [AoMember(1)]
+        public bool AutoSit { get; set; }
+        [AoMember(2)]
+        public bool MorphPathing { get; set; }
+        [AoMember(3)]
+        public bool BellyPathing { get; set; }
+        [AoMember(4)]
+        public bool Eumenides { get; set; }
+        [AoMember(5)]
+        public bool Db3Shapes { get; set; }
+    }
+}

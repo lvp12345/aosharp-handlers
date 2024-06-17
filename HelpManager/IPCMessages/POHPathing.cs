7b0f872f8d5c9e7c8d2d0468f2c444e2eb377b57
Knowidea <mellowclads@hotmail.com>
2024-06-17 12:17:03 +0000

    Helpmanager: Poh: path to portal mobs and rift timers

---
+++ HelpManager/IPCMessages/POHPathing.cs
@@ -0,0 +1,15 @@
+﻿using AOSharp.Common.GameData;
+using AOSharp.Core.IPC;
+using SmokeLounge.AOtomation.Messaging.Serialization.MappingAttributes;
+
+namespace HelpManager.IPCMessages
+{
+    [AoContract((int)IPCOpcode.POHPathing)]
+    internal class POHPathing : IPCMessage
+    {
+        public override short Opcode => (short)IPCOpcode.POHPathing;
+
+        [AoMember(0)]
+        public Vector3 Position { get; set; }
+    }
+}
